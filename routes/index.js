57257515f04dedccdbe864b7a9a9ad3e97bbb51d
Jonas Auer <jonas.auer.94@gmail.com>
2015-11-21 14:13:53 +0100

    The app now exposes the static fronend files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
-var Router = require('express').Router;
+var express = require('express');
+var path = require('path');
+
+var Router = express.Router;
 var router = new Router();
 
 module.exports = router;
 
 router.use('/login', require('./login'));
 router.use('/api', require('./api'));
+router.use('/', express.static(path.join(__dirname, '../public')));
